efab6ce78805ed388f3a4b4c350cbda2d7ac39b0
Matt Crowe <matthew.p.crowe@gmail.com>
2017-01-27 12:54:49 -0500

    laravel 5.4

--- src/base/Behaviors/FileableTrait.php
+++ src/base/Behaviors/FileableTrait.php
@@ -9,6 +9,19 @@ trait FileableTrait
 
     use MorphToSortedManyTrait;
 
+    /**
+     * @todo deprecate
+     *
+     * Eloquent renamed getBelongsToManyCaller to guessBelongsToManyRelation
+     * and the package Rutorika\Sortable currently expects the old name to exist
+     *
+     * @return mixed
+     */
+    protected function getBelongsToManyCaller()
+    {
+        return $this->guessBelongsToManyRelation();
+    }
+
     public static function getResizePresets()
     {
         return isset(static::$presets) ? static::$presets : [];
