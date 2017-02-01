83f3f8f8580f780740aad3c91b5f5bcfbc899925
rob.lasota <rob.lasota@fahlgren.com>
2017-02-01 14:40:17 -0500

    removing individual script

--- resources/views/base/admin/dashboard.blade.php
+++ resources/views/base/admin/dashboard.blade.php
@@ -1,10 +1,5 @@
 @extends('ohio-core::layouts.admin.main')
 
-@section('scripts-body-close')
-    @parent
-    <script src="/js/ohio-storage.js"></script>
-@endsection
-
 @section('main')
 
     <div id="ohio-storage">
