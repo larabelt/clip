00c1893b3ba6d51350daaa557c9c6486674dfeab
rob.lasota <rob.lasota@fahlgren.com>
2017-02-01 14:14:23 -0500

    updated to use sylink and chnaged to heading.html

--- resources/js/components/file/ctlr-edit.js
+++ resources/js/components/file/ctlr-edit.js
@@ -1,4 +1,4 @@
-import headingTemplate from 'ohio/core/js/templates/base/heading';
+import headingTemplate from 'ohio/core/js/templates/base/heading.html';
 import fileService from './service';
 import fileFormTemplate from './templates/form';
 
--- resources/js/components/file/ctlr-index.js
+++ resources/js/components/file/ctlr-index.js
@@ -1,4 +1,4 @@
-import headingTemplate from 'ohio/core/js/templates/base/heading';
+import headingTemplate from 'ohio/core/js/templates/base/heading.html';
 import fileService from './service';
 import fileIndexTemplate from './templates/index';
 
