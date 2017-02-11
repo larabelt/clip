f4bbd56cb529b89283953ec9a66ce50565a8a3c1
Matt Crowe <github.mpc@zym.me>
2017-02-11 02:38:57 +0000

    pluralize view folder names

--- resources/views/files/sections/default.blade.php
+++ resources/views/files/sections/default.blade.php
@@ -5,6 +5,6 @@
 <div class="section section-file {{ $section->param('class') }}">
     @include('ohio-content::section.sections._header')
     @include('ohio-content::section.sections._body')
-    @include('ohio-storage::file.web._show')
+    @include('ohio-storage::files.web._show')
     @include('ohio-content::section.sections._footer')
 </div>
