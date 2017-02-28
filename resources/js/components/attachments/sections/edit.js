b475b88913d189f6d5c7e005012fc8984fd242de
Matt Crowe <github.mpc@zym.me>
2017-02-28 19:40:30 +0000

    page sections UI

---
+++ resources/js/components/attachments/sections/edit.js
@@ -0,0 +1,42 @@
+// components
+import shared from 'belt/content/js/components/sectionables/ctlr/panel-shared';
+
+// helpers
+import Form from '../form';
+import Table from '../table';
+
+// templates
+import edit_html from './edit.html';
+
+export default {
+    mixins: [shared],
+    data() {
+        return {
+            table: new Table({query: {perPage: 5}}),
+            attachment: new Form(),
+        }
+    },
+    mounted() {
+        if (this.section.sectionable_id) {
+            this.attachment.show(this.section.sectionable_id);
+        }
+    },
+    methods: {
+        update(id)
+        {
+            let form = this.panel.form;
+            let attachment = this.attachment;
+            let table = this.table;
+
+            form.sectionable_id = id;
+
+            form.submit()
+                .then(function () {
+                    table.query.q = '';
+                    table.items = [];
+                    attachment.show(form.sectionable_id);
+                });
+        }
+    },
+    template: edit_html
+}
