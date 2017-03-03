d2518d535c7b044a5ccd30c43fe6499f55c50a6a
Matt Crowe <github.mpc@zym.me>
2017-03-03 17:11:08 +0000

    better section template for attachments

--- resources/views/attachments/sections/default.blade.php
+++ resources/views/attachments/sections/default.blade.php
@@ -2,9 +2,11 @@
     $attachment = $section->sectionable;
 @endphp
 
-<div class="section section-attachment {{ $section->param('class') }}">
-    @include('belt-content::sections.sections._heading')
-    @include('belt-content::sections.sections._before')
-    @include('belt-clip::attachments.web.show')
-    @include('belt-content::sections.sections._after')
-</div>
+@if ($attachment)
+    <div class="section section-attachment {{ $section->param('class') }}">
+        @include('belt-content::sections.sections._heading')
+        @include('belt-content::sections.sections._before')
+        @include('belt-clip::attachments.web.show')
+        @include('belt-content::sections.sections._after')
+    </div>
+@endif
