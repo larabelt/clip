30220ea8fe145408e2a22078cb04c39abac8aed8
Matt Crowe <github.mpc@zym.me>
2017-04-19 19:00:22 +0000

    album factory tweak

--- database/factories/BeltClipAlbumFactory.php
+++ database/factories/BeltClipAlbumFactory.php
@@ -1,22 +1,9 @@
 <?php
 
-use Belt\Core\Helpers\FactoryHelper;
-
 $factory->define(Belt\Clip\Album::class, function (Faker\Generator $faker) {
 
-    if (!isset(FactoryHelper::$ids['teams'])) {
-        FactoryHelper::$ids['teams'] = Belt\Core\Team::get(['id'])->pluck('id')->toArray();
-    }
-
-    if (!isset(FactoryHelper::$ids['users'])) {
-        FactoryHelper::$ids['users'] = Belt\Core\User::get(['id'])->pluck('id')->toArray();
-    }
-
     return [
-        'user_id' => $faker->randomElement(FactoryHelper::$ids['users']),
-        'team_id' => $faker->randomElement(FactoryHelper::$ids['teams']),
         'name' => $faker->words(random_int(1, 2), true),
-        'intro' => $faker->paragraphs(1, true),
         'body' => $faker->paragraphs(3, true),
     ];
 });
