ac1f4d5eb539f7a814abdb3646a160e8d14b2331
Matt Crowe <github.mpc@zym.me>
2017-04-28 16:20:20 +0000

    taggable albums

--- resources/js/components/albums/ctlr/shared.js
+++ resources/js/components/albums/ctlr/shared.js
@@ -1,9 +1,19 @@
+// templates
+import heading_html from 'belt/core/js/templates/heading.html';
+import tabs_html from '../templates/tabs.html';
+import edit_html from '../templates/edit.html';
+
 export default {
     data() {
         return {
             morphable_type: 'albums',
-            morphable_id: this.$parent.morphable_id,
+            morphable_id: this.$route.params.id,
             album: this.$parent.album,
         }
     },
+    components: {
+        heading: {template: heading_html},
+        tabs: {template: tabs_html},
+    },
+    template: edit_html,
 }
--- resources/js/components/albums/routes.js
+++ resources/js/components/albums/routes.js
@@ -2,10 +2,12 @@ import index from './ctlr/index';
 import create from './ctlr/create';
 import edit  from './ctlr/edit';
 import attachments  from './ctlr/attachments';
+import tags  from './ctlr/tags';
 
 export default [
     {path: '/albums', component: index, canReuse: false, name: 'albums'},
     {path: '/albums/create', component: create, name: 'albums.create'},
     {path: '/albums/edit/:id', component: edit, name: 'albums.edit'},
     {path: '/albums/edit/:id/attachments', component: attachments, name: 'albums.attachments'},
+    {path: '/albums/edit/:id/tags', component: tags, name: 'albums.tags'},
 ]
