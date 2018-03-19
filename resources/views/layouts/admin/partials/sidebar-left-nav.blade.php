049612676f4d20fd352f2c7f14c16498afd22ee8
Matt Crowe <github.mpc@zym.me>
2018-03-19 14:38:56 +0000

    sidebar nav ids

--- resources/views/layouts/admin/partials/sidebar-left-nav.blade.php
+++ resources/views/layouts/admin/partials/sidebar-left-nav.blade.php
@@ -4,16 +4,16 @@
 @endphp
 
 @if($can['albums'] || $can['attachments'])
-    <li class="treeview">
+    <li id="clip-admin-sidebar-left" class="treeview">
         <a href="#">
             <i class="fa fa-paperclip"></i> <span>Clip Admin</span> <i class="fa fa-angle-left pull-right"></i>
         </a>
         <ul class="treeview-menu">
             @if($can['albums'])
-                <li><a href="/admin/belt/clip/albums"><i class="fa fa-files-o"></i> <span>Albums</span></a></li>
+                <li id="clip-admin-sidebar-left-albums"><a href="/admin/belt/clip/albums"><i class="fa fa-files-o"></i> <span>Albums</span></a></li>
             @endif
             @if($can['attachments'])
-                <li><a href="/admin/belt/clip/attachments"><i class="fa fa-file-o"></i> <span>Attachments</span></a></li>
+                <li id="clip-admin-sidebar-left-attachments"><a href="/admin/belt/clip/attachments"><i class="fa fa-file-o"></i> <span>Attachments</span></a></li>
             @endif
         </ul>
     </li>
