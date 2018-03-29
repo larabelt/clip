e56701fbdd1ce651890001a27b6b6ae1ee11fe9b
Matt Crowe <github.mpc@zym.me>
2018-03-29 21:49:32 +0000

    sections UX continued

--- resources/belt/clip/js/clip.js
+++ resources/belt/clip/js/clip.js
@@ -2,6 +2,9 @@ import albums from 'belt/clip/js/albums/routes';
 import attachments from 'belt/clip/js/attachments/routes';
 import store from 'belt/core/js/store/index';
 
+import inputAttachment from 'belt/clip/js/inputs/attachment';
+Vue.component('input-attachment', inputAttachment);
+
 window.larabelt.clip = _.get(window, 'larabelt.clip', {});
 
 export default class BeltClip {
---
+++ resources/belt/clip/js/inputs/attachment/index.js
@@ -0,0 +1,38 @@
+import shared from 'belt/core/js/inputs/shared';
+import AttachmentTable from 'belt/clip/js/attachments/table';
+import AttachmentForm from 'belt/clip/js/attachments/form';
+import thumb from 'belt/clip/js/attachments/thumb';
+import html from 'belt/clip/js/inputs/attachment/template.html';
+
+export default {
+    mixins: [shared],
+    data() {
+        return {
+            attachment: new AttachmentForm(),
+            attachments: new AttachmentTable({query: {perPage: 20}}),
+        };
+    },
+    created() {
+        this.config.label = _.get(this.config, 'label', 'Attachment');
+        this.config.description = _.get(this.config, 'description', 'Use the search field to find attachments that can be linked to this item.');
+        this.$watch('form.' + this.column, function (newValue) {
+            this.attachment.show(newValue);
+        });
+    },
+    mounted() {
+        if (this.value) {
+            this.attachment.show(this.value);
+        }
+    },
+    methods: {
+        clear() {
+            this.attachments.query.q = '';
+        },
+        update(id) {
+            this.form[this.column] = id;
+            this.emitEvent();
+        },
+    },
+    components: {thumb},
+    template: html
+}
