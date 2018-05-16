dd0ecee5af7f80828641721048315447cca09427
Matt Crowe <github.mpc@zym.me>
2018-05-16 19:36:46 +0000

    breadcrumb updates

--- resources/belt/clip/js/albums/ctlr/shared.js
+++ resources/belt/clip/js/albums/ctlr/shared.js
@@ -1,19 +1,20 @@
-// templates
-
+import Form from 'belt/clip/js/albums/form';
 import tabs_html from 'belt/clip/js/albums/templates/tabs.html';
-import edit_html from 'belt/clip/js/albums/templates/edit.html';
+import html from 'belt/clip/js/albums/templates/edit.html';
 
 export default {
     data() {
         return {
             morphable_type: 'albums',
             morphable_id: this.$route.params.id,
-            album: this.$parent.album,
+            album: new Form(),
         }
     },
+    mounted() {
+        this.album.show(this.morphable_id);
+    },
     components: {
-
         tabs: {template: tabs_html},
     },
-    template: edit_html,
+    template: html,
 }
