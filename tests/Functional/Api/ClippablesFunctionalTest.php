50d02e5980044c56cb29edf9899bce4f46442228
Matt Crowe <github.mpc@zym.me>
2018-06-25 19:52:32 +0000

    tests

--- tests/Functional/Api/ClippablesFunctionalTest.php
+++ tests/Functional/Api/ClippablesFunctionalTest.php
@@ -1,6 +1,10 @@
 <?php
 
+use Silber\Bouncer\BouncerFacade as Bouncer;
 use Belt\Core\Testing;
+use Belt\Core\User;
+use Belt\Clip\Attachment;
+use Belt\Clip\Album;
 
 class ClippablesFunctionalTest extends Testing\BeltTestCase
 {
@@ -11,37 +15,63 @@ class ClippablesFunctionalTest extends Testing\BeltTestCase
         $this->actAsSuper();
 
         # index
-        $response = $this->json('GET', '/api/v1/places/1/attachments');
+        $response = $this->json('GET', '/api/v1/albums/1/attachments');
         $response->assertStatus(200);
 
         # attach
-        $response = $this->json('POST', '/api/v1/places/1/attachments', ['id' => 1]);
+        $response = $this->json('POST', '/api/v1/albums/1/attachments', ['id' => 11]);
         $response->assertStatus(201);
-        $response = $this->json('GET', "/api/v1/places/1/attachments/1");
+        $response = $this->json('GET', "/api/v1/albums/1/attachments/11");
+        $position11 = $response->json('position');
         $response->assertStatus(200);
         $response->assertJsonFragment(['id']);
 
         # attach (fail)
-        $response = $this->json('POST', '/api/v1/places/1/attachments', ['id' => 1]);
+        $response = $this->json('POST', '/api/v1/albums/1/attachments', ['id' => 11]);
         $response->assertStatus(422);
 
-        # update (position: add attachement #2 and then move before #1)
-        $this->json('POST', '/api/v1/places/1/attachments', ['id' => 2]);
-        $response = $this->json('GET', "/api/v1/places/1/attachments/2");
-        $response->assertJson(['position' => 2]);
-        $this->json('PUT', '/api/v1/places/1/attachments/2', ['move' => 'before', 'position_entity_id' => 1]);
-        $response = $this->json('GET', "/api/v1/places/1/attachments/2");
-        $response->assertJson(['position' => 1]);
+        # update (position: add attachement #12 and then move before #11)
+        $this->json('POST', '/api/v1/albums/1/attachments', ['id' => 12]);
+        $response = $this->json('GET', "/api/v1/albums/1/attachments/12");
+        $position12 = $response->json('position');
+        $response->assertJson(['position' => $position11 + 1]);
+        $this->json('PUT', '/api/v1/albums/1/attachments/12', ['move' => 'before', 'position_entity_id' => 11]);
+        $response = $this->json('GET', "/api/v1/albums/1/attachments/11");
+        $response->assertJson(['position' => $position12]);
 
         # show
-        $response = $this->json('GET', "/api/v1/places/1/attachments/1");
+        $response = $this->json('GET', "/api/v1/albums/1/attachments/11");
         $response->assertStatus(200);
 
         # detach
-        $response = $this->json('DELETE', "/api/v1/places/1/attachments/1");
+        $response = $this->json('DELETE', "/api/v1/albums/1/attachments/11");
         $response->assertStatus(204);
-        $response = $this->json('DELETE', "/api/v1/places/1/attachments/1");
+        $response = $this->json('DELETE', "/api/v1/albums/1/attachments/11");
         $response->assertStatus(422);
+    }
+
+    public function testNotSuper()
+    {
+        $this->refreshDB();
+
+        $album = Album::find(1);
+        $attachment = Attachment::find(1);
+        $album->attachments()->syncWithoutDetaching(1);
+
+        # update (authorize exception)
+        User::unguard();
+        $user = factory(User::class)->create(['is_active' => true]);
+        $this->actingAs($user);
+
+        Bouncer::allow($user)->to('update', Album::class);
+        Bouncer::disallow($user)->to('update', Attachment::class);
+        Bouncer::refreshFor($user);
+
+        $response = $this->json('PUT', '/api/v1/albums/1/attachments/1', ['title' => 'foo']);
+
+        $response->assertJson(['title' => $attachment->title]);
+
+
     }
 
 }
